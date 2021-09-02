0cf591561eb53b3780848d88d7b4b8a00c176ff0
Harsh Khajgiwale <13365920+hkhajgiwale@users.noreply.github.com>
2021-09-02 23:44:39 +0530

    Update TwitterConfig.java

--- SPAGroup035TwitterProducer/src/main/java/org/group10/twitter/config/TwitterConfig.java
+++ SPAGroup035TwitterProducer/src/main/java/org/group10/twitter/config/TwitterConfig.java
@@ -1,9 +1,9 @@
 package org.group10.twitter.config;
 
 public class TwitterConfig {
-	public static String CONSUMER_KEY = "cmWZXo4O12cTsTTMaZp9mxGjN";
-	public static String CONSUMER_SECRET = "zEexnPtQH0XTNj6NAsPMJJFGuUs8b6Iy1Gi41FiPlI0VcZNNA4";
-	public static String ACCESS_TOKEN = "1017765831130959873-OWFGoa8z8vTBsoz74KIrWjdNrOimVB";
-	public static String ACCESS_SECRET = "uAHmYHTq7WLS0ie6EHM1PWrfPjEt9lMPlUGD9VqCMA2H2";
+	public static String CONSUMER_KEY = "XXXXXXXXXXXX";
+	public static String CONSUMER_SECRET = "XXXXXXXXXXXXXXXXx";
+	public static String ACCESS_TOKEN = "XXXXXXXXXXXXXXXXXX";
+	public static String ACCESS_SECRET = "XXXXXXXXXXXxx";
 	public static String TERM = "#covid19";
 }
